60023def994d0bcc42c606059f0e10cc15210529
SkieLim <skienemoculo@gmail.com>
2024-03-22 21:28:06 -0300

    lista-while

---
+++ c-folders/lista-while/ex1.cpp
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    int x
+    ;
+x = 1;
+    while (x <= 50){
+        if(x % 2 == 0){
+
+            printf("%i \n", x);
+
+        }
+        x++;
+    }
+    return 0;
+}
---
+++ c-folders/lista-while/ex2.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+    float num;
+    
+    while (num >= 0){
+        printf("Digite um numero\n");
+            scanf("%f", &num);
+        
+    }
+    return 0;
+}
